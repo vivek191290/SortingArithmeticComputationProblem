fd22dbcf7fb7d879eaca7cafd9a094a73c956c54
“vivek191290” <vivekpatil9525@gmail.com>
2023-03-06 10:37:50 +0530

    Add: Computed a + b * c

--- arithmeticComputionAndSorting.sh
+++ arithmeticComputionAndSorting.sh
@@ -3,3 +3,5 @@ echo "Welcome to Arithmetic Computation & Sorting program"
 read -p "Enter fist number : " a
 read -p "Enter second number : " b
 read -p "Enter third number : " c
+
+result=$((a+b*c))
