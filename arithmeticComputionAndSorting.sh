d7c68cdd51bfa68fc4c7b0dbd5c6c5d3f9ba4de3
“vivek191290” <vivekpatil9525@gmail.com>
2023-03-06 11:07:23 +0530

    Add: Store the results in a Dictionary for every Computation

--- arithmeticComputionAndSorting.sh
+++ arithmeticComputionAndSorting.sh
@@ -1,10 +1,15 @@
 echo "Welcome to Arithmetic Computation & Sorting program"
 
+declare -A result
+
 read -p "Enter fist number : " a
 read -p "Enter second number : " b
 read -p "Enter third number : " c
 
-result=$((a+b*c))
-result=$((a*b+c))
-result=$((c+a/b))
-result=$((a%b+c))
+
+result[1]=$((a+b*c))
+result[2]=$((a*b+c))
+result[3]=$((c+a/b))
+result[4]=$((a%b+c))
+
+echo "Stored the all computation result in dectionary " ${result[@]}
